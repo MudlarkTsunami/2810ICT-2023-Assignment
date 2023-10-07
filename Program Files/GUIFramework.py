43767f142be2b41e447fa26638e1cb29785ab698
MudlarkTsunami <hjbaker2@hotmail.com>
2023-10-07 15:10:29 +1000

    Added a folder where images will be generated and stored, added code to show a popup that will display the price chart requirement

--- Program Files/GUIFramework.py
+++ Program Files/GUIFramework.py
@@ -1,16 +1,20 @@
 import datetime
+import os.path
+
 import PySimpleGUI as sg
 
-#Changing the default theme to a darker one
+# Changing the default theme to a darker one
 sg.theme('DarkAmber')
+# Default value of the chart image points to an error image
+chartImage = 'img/Dizzy.png'
 
-#Left hand column, contains all the buttons
+# Left hand column, contains all the buttons
 ButtonSelectColumn = [
-    [sg.CalendarButton(button_text='date from', key='dateToBtn', target='dateToTxt', format="%Y-%m-%d"),
-     sg.Text(text=datetime.date.today(), key='dateToTxt')],
-    [sg.Text('to')],
-    [sg.CalendarButton(button_text='date to', key='dateFromBtn', target='dateFromTxt', format="%Y-%m-%d"),
+    [sg.CalendarButton(button_text='date from', key='dateToBtn', target='dateFromTxt', format="%Y-%m-%d"),
      sg.Text(text=datetime.date.today(), key='dateFromTxt')],
+    [sg.Text('to')],
+    [sg.CalendarButton(button_text='date to', key='dateFromBtn', target='dateToTxt', format="%Y-%m-%d"),
+     sg.Text(text=datetime.date.today(), key='dateToTxt')],
     [sg.HSeparator()],
     [sg.Button(button_text='by Suburb')],
     [sg.HSeparator()],
@@ -22,7 +26,7 @@ ButtonSelectColumn = [
     [sg.Button(button_text='by Name')],
     [sg.InputText()],
     [sg.HSeparator()],
-    [sg.Button(button_text='by Price')]
+    [sg.Button(button_text='by Price', key='priceBtn')]
 ]
 
 # Right hand column, will contain the generated chart images
@@ -31,8 +35,8 @@ ImageColumn = [
     [sg.Text('Test text 2')]
 ]
 
-# Window layout, left column, vertical separator and right column
-layout = [
+# Main window layout, left column, vertical separator and right column
+mainLayout = [
     [
         sg.Column(ButtonSelectColumn),
         sg.VSeparator(),
@@ -40,15 +44,28 @@ layout = [
     ]
 ]
 
+
 # Creating new window
-window = sg.Window('DigiBird Eyeview v0.01', layout)
+mainWindow = sg.Window('DigiBird Eyeview v0.01', mainLayout)
+
+def SetChartImage(dateFrom, dateUntil):
+    print(dateFrom, dateUntil)
+    global chartImage
+    # setting the desired path to find the chart image
+    chartImage = ('img/' + mainWindow['dateFromTxt'].get() + '-' + mainWindow['dateToTxt'].get() + 'Price.png')
+    # Checking if that image exists, if it doesn't, defaulting to an error image
+    if not os.path.isfile(chartImage):
+        chartImage = "img/Dizzy.png"
 
 # Loop to process events while application is running
 while True:
-    event, values = window.read()
+    event, values = mainWindow.read()
+    if event == "priceBtn":
+        SetChartImage('ok', 'hello')
+        sg.popup('meowdy', image=chartImage)
     if event == sg.WIN_CLOSED: # if user closes window
         print('Application exited')
         break
 
 
-window.close()
+mainWindow.close()
