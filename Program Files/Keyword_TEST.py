60413aa6b575c98c1fc3b1a827525dc69575c58e
s5134726 <68205885+s5134726@users.noreply.github.com>
2023-10-09 17:06:35 +1000

    Updated Test Complete (PASSED)

--- Program Files/Keyword_TEST.py
+++ Program Files/Keyword_TEST.py
@@ -1,63 +1,46 @@
-#Imports
-
-import pytest
 import pandas as pd
-import Keyword ip
+import pytest
+import Keyword as kw
 
-# Sample data for testing
-s_cal_data = pd.DataFrame({
-    'date': pd.to_datetime(['2022-01-01', '2022-01-02', '2022-01-03']),
-    'listing_id': [1, 2, 3],
-})
-
-sample_listings_data = pd.DataFrame({
+# Test data
+t_data_listings = pd.DataFrame({
     'id': [1, 2, 3],
     'summary': ['Nice place', 'Another place', 'Yet another place'],
     'space': ['Small', 'Medium', 'Large'],
     'description': ['Cozy', 'Affordable', 'Luxurious'],
 })
 
+t_data_calendar = pd.DataFrame({
+    'date': pd.to_datetime(['2022-01-01', '2022-01-02', '2022-01-03']),
+    'listing_id': [1, 2, 3],
+})
 
-# Tests
-def t_keyword_f_listings():
-    f_data = ip.keyword_filtered_listings(
-        sample_listings_data, 'Nice', ['summary', 'description'])
+# Test keyword_filtered_listings: Check function filters  listings in keyword.
+def t_keyword_filtered_listings():
+    res_df = kw.keyword_filtered_listings(t_data_listings, 'Nice', ['summary', 'space', 'description'])
+    assert all(res_df['summary'].apply(lambda x: 'Nice' in x))
 
-    # Assertions
-    assert len(f_data) == 1, "Incorrect number of rows in the keyword filtered data"
-    assert 'Nice place' in f_data['summary'].values, "Expected keyword not found in summary"
+# Test date_filtered_listings: function filters listings in  date range.
+def t_date_filtered_listings():
+    res_df = kw.date_filtered_listings(t_data_calendar, '2022-01-01', '2022-01-02')
+    assert all((res_df['date'] > '2022-01-01') & (res_df['date'] < '2022-01-02'))
 
-
-def t_date_f_listings():
-    f_data = ip.date_filtered_listings(s_cal_data, '2022-01-01', '2022-01-03')
-
-    # Assertions
-    assert len(f_data) == 3, "Incorrect number of rows in the filtered data"
-    assert f_data['date'].min() == pd.to_datetime('2022-01-01'), "Incorrect minimum date"
-    assert f_data['date'].max() == pd.to_datetime('2022-01-03'), "Incorrect maximum date"
-
-
+# Test find_keyword_listings: Check function merges filtered dataframes properly .
 def t_find_keyword_listings():
-    k_f_data = ip.find_keyword_listings(
-        sample_listings_data, s_cal_data, 'Nice', '2022-01-01', '2022-01-03')
-
-    # Assertions
-    assert len(k_f_data) == 1, "Incorrect number of rows in the keyword filtered data"
-    assert 'Nice place' in k_f_data['summary'].values, "Expected keyword not found in summary"
-    assert k_f_data['id'].values[0] == 1, "Incorrect id for the keyword filtered data"
-
-@pytest.mark.parametrize("test_func", [t_keyword_f_listings, t_date_f_listings, t_find_keyword_listings])
-def t_run(t_func):
-    t_func()
+    res_df = kw.find_keyword_listings(t_data_listings, t_data_calendar, 'Nice', '2022-01-01', '2022-01-03')
+    assert all(res_df['summary'].apply(lambda x: 'Nice' in x))
+    assert all((res_df['date'] > '2022-01-01') & (res_df['date'] < '2022-01-03'))
 
 # Execute tests
 t_res = {"Passed": [], "Failed": []}
 
-for t_func in [t_keyword_f_listings, t_date_f_listings, t_find_keyword_listings]:
+for t_func in [t_keyword_filtered_listings, t_date_filtered_listings, t_find_keyword_listings]:
     try:
-        t_run(t_func)
+        t_func()
+        print(f"{t_func.__name__}: Passed")
         t_res["Passed"].append(t_func.__name__)
-    except AssertionError:
+    except AssertionError as e:
+        print(f"{t_func.__name__}: Failed - {str(e)}")
         t_res["Failed"].append(t_func.__name__)
 
 t_res
